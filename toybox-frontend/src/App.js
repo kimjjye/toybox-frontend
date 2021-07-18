0bc89a77c240338d67fa090a23599f730a144381
alvin <jjye81930@gmail.com>
2021-07-18 23:16:53 +0900

    set front-end architecture

---
+++ toybox-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Noelvin's Toy Project!
+      </header>
+    </div>
+  );
+}
+
+export default App;
